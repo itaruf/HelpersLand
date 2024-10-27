07730d41a02356680a0da4763c6ad66e9b9cf393
itaruf <79866301+itaruf@users.noreply.github.com>
2024-10-27 19:05:57 +0900

    feat: add a health component to serve as a base class

---
+++ Source/HelpersLand/ActorComponents/Health/HealthComponent.cpp
@@ -0,0 +1,101 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+}
+
+int UHealthComponent::GetHealth() const
+{
+	return Health;
+}
+
+void UHealthComponent::SetHealth(const int NewHealth)
+{
+	Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
+}
+
+int UHealthComponent::GetMaxHealth() const
+{
+	return MaxHealth;
+}
+
+void UHealthComponent::SetMaxHealth(const int NewMaxHealth)
+{
+	if (NewMaxHealth < 0)
+		MaxHealth = 0;
+	else
+		MaxHealth = NewMaxHealth;
+}
+
+float UHealthComponent::GetHealthRegenerationRate() const
+{
+	return HealthRegenerationRate;
+}
+
+void UHealthComponent::SetHealthRegenerationRate(const float NewHealthRegenerationRate)
+{
+	if (NewHealthRegenerationRate < 0)
+		HealthRegenerationRate = 0;
+	else
+		HealthRegenerationRate = NewHealthRegenerationRate;
+}
+
+float UHealthComponent::GetHealthDecayRate() const
+{
+	return HealthDecayRate;
+}
+
+void UHealthComponent::SetHealthDecayRate(const float NewHealthDecayRate)
+{
+	if (NewHealthDecayRate < 0)
+		HealthDecayRate = 0;
+	else
+		HealthDecayRate = NewHealthDecayRate;
+}
+
+bool UHealthComponent::IsAlive() const
+{
+	return Health > 0;
+}
+
+void UHealthComponent::SetIsAlive(const bool NewIsAlive)
+{
+	bIsAlive = NewIsAlive;
+}
+
+bool UHealthComponent::IsInvincible() const
+{
+	return bIsInvincible;
+}
+
+void UHealthComponent::SetIsInvincible(const bool NewIsInvincible)
+{
+	bIsInvincible = NewIsInvincible;
+}
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
---
+++ Source/HelpersLand/ActorComponents/Health/HealthComponent.h
@@ -0,0 +1,94 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent, name="Health Component"))
+class HELPERSLAND_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health", meta = (ClampMin = "0", Tooltip="The current health value of the actor"))
+	int Health = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health", meta = (ClampMin = "0", Tooltip = "The current max health value of the actor"))
+	int MaxHealth = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health|Regeneration", meta = (ClampMin = "0", Tooltip = "The current health regeneration rate of the actor"))
+	float HealthRegenerationRate = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health|Regeneration", meta = (ClampMin = "0", Tooltip = "The current health decay rate of the actor"))
+	float HealthDecayRate = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status", meta = (Tooltip = "True if the actor is alive"))
+	bool bIsAlive = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status", meta = (Tooltip = "True if the actor is invincible"))
+	bool bIsInvincible = false;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Getters & Setters
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Health Component", meta = (DisplayName = "GetHealth"))
+	int GetHealth() const;
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "SetHealth"))
+	void SetHealth(const int NewHealth);
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Health Component", BlueprintPure, meta = (DisplayName = "GetMaxHealth"))
+	int GetMaxHealth() const;
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "SetMaxHealth"))
+	void SetMaxHealth(const int NewMaxHealth);
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "GetHealthRegenerationRate"))
+	float GetHealthRegenerationRate() const;
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "SetHealthRegenerationRate"))
+	void SetHealthRegenerationRate(const float NewHealthRegenerationRate);
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "GetHealthDecayRate"))
+	float GetHealthDecayRate() const;
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "SetHealthDecayRate"))
+	void SetHealthDecayRate(const float NewHealthDecayRate);
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "IsAlive"))
+	bool IsAlive() const;
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "SetHealthDecayRate"))
+	void SetIsAlive(const bool NewIsAlive);
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "IsInvincible"))
+	bool IsInvincible() const;
+	UFUNCTION(BlueprintCallable, Category = "Health Component", meta = (DisplayName = "SetIsInvincible"))
+	void SetIsInvincible(const bool NewIsInvincible);
+
+public:
+	// Event Dispatchers
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChanged, int, NewHealth);
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Events")
+	FOnHealthChanged OnHealthChanged;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChanged, int, NewMaxHealth);
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Events")
+	FOnMaxHealthChanged OnMaxHealthChanged;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthRegenerationRateChanged, float, NewHealthRegenerationRate);
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Events")
+	FOnHealthRegenerationRateChanged OnHealthRegenerationRateChanged;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAliveStatusChanged, bool, NewAlive);
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Events")
+	FOnAliveStatusChanged OnAliveStatusChanged;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInvincibleStatusChanged, bool, NewInvincible);
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Events")
+	FOnInvincibleStatusChanged OnInvincibleStatusChanged;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
